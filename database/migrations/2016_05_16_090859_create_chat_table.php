be51271f782722f4d37633ecf1950ad73f1962b6
JeremyBellaiche <contact@jeremybellaiche.com>
2016-05-16 12:08:50 +0100

    Fix table

--- database/migrations/2016_05_16_090859_create_chat_table.php
+++ database/migrations/2016_05_16_090859_create_chat_table.php
@@ -16,22 +16,22 @@ class CreateChatTable extends Migration
         Schema::create('chats', function(Blueprint $table){
             $table->increments('id');
             $table->string('title');
-            $table->integer('fk_owner_id')->unsigned();
-            $table->integer('fk_last_entry')->unsigned();
+            $table->integer('fk_owner_id')->unsigned()->nullable();
+            $table->integer('fk_last_entry')->unsigned()->nullable();
             $table->timestamps();
         });
 
         Schema::create('chat_users', function(Blueprint $table){
             $table->increments('id');
-            $table->integer('fk_chat_id')->unsigned();
-            $table->integer('fk_user_id')->unsigned();
-            $table->integer('fk_last_message_seen')->unsigned();
+            $table->integer('fk_chat_id')->unsigned()->nullable();
+            $table->integer('fk_user_id')->unsigned()->nullable();
+            $table->integer('fk_last_message_seen')->unsigned()->nullable();
             $table->timestamps();
         });
 
         Schema::create('devices', function(Blueprint $table){
             $table->increments('id');
-            $table->integer('fk_user_id')->unsigned();
+            $table->integer('fk_user_id')->unsigned()->nullable();
             $table->string('msisdn');
             $table->string('user_agent');
             $table->timestamps();
@@ -39,15 +39,15 @@ class CreateChatTable extends Migration
 
         Schema::create('presence', function(Blueprint $table){
             $table->increments('id');
-            $table->integer('fk_user_id')->unsigned();
+            $table->integer('fk_user_id')->unsigned()->nullable();
             $table->timestamp('last_seen');
             $table->timestamps();
         });
 
         Schema::create('messages', function(Blueprint $table){
             $table->increments('id');
-            $table->integer('fk_chat_id')->unsigned();
-            $table->integer('fk_user_id')->unsigned();
+            $table->integer('fk_chat_id')->unsigned()->nullable();
+            $table->integer('fk_user_id')->unsigned()->nullable();
             $table->tinyInteger('msg_type');
             $table->text('msg_text')->nullable();
             $table->string('msg_attachment')->nullable();
@@ -57,7 +57,7 @@ class CreateChatTable extends Migration
         // Foreign Keys
 
         Schema::table('users', function(Blueprint $table){
-            $table->integer('fk_device_id')->unsigned();
+            $table->integer('fk_device_id')->unsigned()->nullable();
             $table->foreign('fk_device_id')->references('id')->on('devices');
         });
 
@@ -96,6 +96,17 @@ class CreateChatTable extends Migration
      */
     public function down()
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::drop('chats');
+        Schema::drop('chat_users');
+        Schema::drop('devices');
+        Schema::drop('presence');
+        Schema::drop('messages');
+        Schema::table('users', function(Blueprint $table){
+            $table->dropForeign('fk_device_id');
+            $table->dropColumn('fk_device_id');
+        });
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
     }
 }
