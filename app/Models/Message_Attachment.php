58be4b7ca55ca76de76e41dcf445e3d8d4265506
JeremyBellaiche <contact@jeremybellaiche.com>
2016-06-01 14:46:19 +0100

    Try fix problem

--- app/Models/Message_Attachment.php
+++ app/Models/Message_Attachment.php
@@ -8,6 +8,6 @@ class Message_Attachment extends Model
 {
 	protected $table = 'messages_attachments';
 
-    protected $fillable = ['path', 'fk_user_id', 'fk_message_id'];
+    protected $fillable = ['url', 'fk_user_id', 'fk_message_id'];
 
 }
