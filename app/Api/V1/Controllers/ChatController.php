7e84325bfd4a36b5125770cc78481c967248da28
JeremyBellaiche <contact@jeremybellaiche.com>
2016-06-01 14:30:23 +0100

    Try fix problem

--- app/Api/V1/Controllers/ChatController.php
+++ app/Api/V1/Controllers/ChatController.php
@@ -76,6 +76,8 @@ class ChatController extends Controller
 
 
         foreach ($request->get('users') as $friend) {
+            return $friend;
+
             if(isset($friend->id)){            
                 Chat_User::create([
                     'fk_chat_id' => $chat->id,
